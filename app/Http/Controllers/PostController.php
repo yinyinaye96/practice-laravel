59d767d2ab934c7ed58fd8d586274cd6bc820e62
yinyinaye96 <yinyinaye.scm@gmail.com>
2021-03-11 23:04:12 +0630

    Section 3 | Hello Eloquent

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use DB;
 
 class PostController extends Controller
 {
     public function show($slug)
     {
-        $post = \DB::table('posts')->where('slug', $slug)->first();
+        $post = Post::where('slug', $slug)->first();
+        if (!$post) {
+            abort(404);
+        }
         return view('post', [
             'post' => $post
         ]);
