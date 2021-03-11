feef68d312e2d23340a95cb4cf57daab4403385f
yinyinaye96 <yinyinaye.scm@gmail.com>
2021-03-11 21:01:18 +0630

    Section 2 | Pass Request Data to Views

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Routing</title>
+    <title>Pass Request Data to Views</title>
 </head>
 <body>
-    <h1>Testing Completing</h1>
+    <h1>{{ $name }}</h1>
 </body>
 </html>
